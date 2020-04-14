dbd48ddd40fbe6d037fb35d9366a70ea2e611d20
Tyler Knapp <tgknapp11@gmail.com>
2020-04-14 10:20:33 -0700

    cards extracted to components

--- av/ContentView.swift
+++ av/ContentView.swift
@@ -11,47 +11,124 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            VStack {
-                Spacer()
-            }
-            .frame(width: 300, height: 220)
-            .background(Color.blue)
-            .cornerRadius(20)
-            .shadow(radius: 20)
-            .offset(x: 0, y: -20.0)
+            TitleView()
+                .blur(radius: 20)
             
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Anna Vient")
-                            .font(.title)
-                            .fontWeight(.black)
-                            .foregroundColor(.white)
-                        Text("Trainer")
-                            .foregroundColor(Color("accent"))
-                    }
-                    Spacer()
-                    Image("Logo1")
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 20)
-                Spacer()
-                Image("Card1")
-                    .resizable().aspectRatio(contentMode: .fit)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 300, height: 110, alignment: .top)
-            }
-            .frame(width: 340.0, height: 220.0)
-            .background(Color.black)
+            
+            BackCardView()
+            .background(Color("card4"))
             .cornerRadius(20)
             .shadow(radius: 20)
+            .offset(x: 0, y: -40)
+            .scaleEffect(0.9)
+            .rotationEffect(.degrees(10))
+            .rotation3DEffect(.init(degrees: 10.0), axis: (x: 10.0, y: 0.0, z: 0.0))
+            .blendMode(.hardLight)
+            
+            BackCardView()
+            .background(Color("card3"))
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            .scaleEffect(0.95)
+            .rotationEffect(.degrees(5))
+            .rotation3DEffect(.init(degrees: 5.0), axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+            
+            CardView()
+            .blendMode(.hardLight)
+            
+            BottomCardView()
+            .blur(radius: 20)
         }
-        
-  }
+    
+     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Anna Vient")
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                    Text("Trainer")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            Spacer()
+            Image("Card1")
+                .resizable().aspectRatio(contentMode: .fit)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 220)
+
+        
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates:")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            
+            Text("Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 500)
     }
 }
