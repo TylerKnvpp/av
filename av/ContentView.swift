a5e7e9adfefcd2c9a2ea6f0ec488a4ca63321bd5
Tyler Knapp <tgknapp11@gmail.com>
2020-04-14 09:11:30 -0700

    z stack cards

--- av/ContentView.swift
+++ av/ContentView.swift
@@ -10,8 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-    }
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20.0)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Anna Vient")
+                            .font(.title)
+                            .fontWeight(.black)
+                            .foregroundColor(.white)
+                        Text("Trainer")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
+                Spacer()
+                Image("Card1")
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+        }
+        
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
