336df6ce0d4aa62b5c04485a07276af5b3d3dfca
Tyler Knapp <tgknapp11@gmail.com>
2020-04-14 10:59:59 -0700

    animations + blurs

--- av/ContentView.swift
+++ av/ContentView.swift
@@ -9,37 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    
+    
     var body: some View {
         ZStack {
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
             
             
             BackCardView()
-            .background(Color("card4"))
+             .background(show ? Color("card4"):  Color("card4"))
             .cornerRadius(20)
             .shadow(radius: 20)
-            .offset(x: 0, y: -40)
+            .offset(x: 0, y: show ? -400 : -40)
             .scaleEffect(0.9)
-            .rotationEffect(.degrees(10))
-            .rotation3DEffect(.init(degrees: 10.0), axis: (x: 10.0, y: 0.0, z: 0.0))
+            .rotationEffect(.degrees(show ? 0 : 10))
+            .rotation3DEffect(Angle(degrees: 10.0), axis: (x: 10.0, y: 0.0, z: 0.0))
             .blendMode(.hardLight)
+            .animation(.spring())
             
             BackCardView()
-            .background(Color("card3"))
+                .background(show ? Color("card4"):  Color("card3"))
             .cornerRadius(20)
             .shadow(radius: 20)
-            .offset(x: 0, y: -20)
+            .offset(x: 0, y: show ? -200 : -20)
             .scaleEffect(0.95)
-            .rotationEffect(.degrees(5))
-            .rotation3DEffect(.init(degrees: 5.0), axis: (x: 10.0, y: 0.0, z: 0.0))
-                .blendMode(.hardLight)
+            .rotationEffect(.degrees(show ? 0 : 5))
+            .rotation3DEffect(Angle(degrees: 5.0), axis: (x: 10.0, y: 0.0, z: 0.0))
+            .blendMode(.hardLight)
+            .animation(.spring())
             
             CardView()
             .blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+            }
             
             BottomCardView()
-            .blur(radius: 20)
+            .blur(radius: show ? 20 : 0)
+            .animation(.default)
         }
     
      }
@@ -96,7 +106,7 @@ struct TitleView: View {
     var body: some View {
         VStack {
             HStack {
-                Text("Certificates:")
+                Text("Posts:")
                     .font(.largeTitle)
                     .fontWeight(.black)
                 
