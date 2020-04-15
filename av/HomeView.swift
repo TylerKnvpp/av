43753db8f6572dfbd45fe0780bbcbc9a382b7bdd
Tyler Knapp <tgknapp11@gmail.com>
2020-04-15 12:49:59 -0700

    rendering cards with dumby data

--- av/HomeView.swift
+++ av/HomeView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 
 struct HomeView: View {
     @Binding var showProfile: Bool
+    
     var body: some View {
         VStack {
             
             HStack {
-                Text("Days")
+                Text("Workouts")
                     .font(.system(size: 28, weight: .bold))
                 
                 Spacer()
@@ -27,8 +28,8 @@ struct HomeView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30) {
-                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                        SectionView()
+                    ForEach(sectionData) { item in
+                        SectionView(section: item)
                     }
                 }
                 .padding(30)
@@ -47,21 +48,24 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 struct SectionView: View {
+    var section: Section
+    
     var body: some View {
         VStack {
             HStack(alignment: .top) {
-                Text("Day one")
+                Text("\(section.title)")
                     .font(.system(size: 24, weight: .bold))
                     .frame(width: 160, alignment: .leading)
                     .foregroundColor(Color.white)
                 Spacer()
-                Image("Logo1")
+                Image("\(section.logo)")
             }
-            Text("18 exercises".uppercased())
+            Text("\(section.text)".uppercased())
+                .foregroundColor(Color(#colorLiteral(red: 0.8490039861, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
                 .frame(maxWidth: .infinity, alignment: .leading)
             
             
-            Image("Card1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -69,8 +73,22 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("card1"))
+        .background(section.color)
         .cornerRadius(30)
-        .shadow(color: Color("card1").opacity(0.5), radius: 20, x: 0, y: 20)
+        .shadow(color: section.color.opacity(0.5), radius: 20, x: 0, y: 20)
     }
 }
+
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(title: "Day One", text: "18 Exercises", logo: "Logo1", image: Image("Card1"), color: Color("card1")), Section(title: "Day Two", text: "10 Exercises", logo: "Logo1", image: Image("Card2"), color: Color("card2")), Section(title: "Day Three", text: "14 Exercises", logo: "Logo1", image: Image("Card3"), color: Color("card3")), Section(title: "Day Four", text: "10 Exercises", logo: "Logo1", image: Image("Card4"), color: Color("card4")), Section(title: "Day Five", text: "12 Exercises", logo: "Logo1", image: Image("Card5"), color: Color("card5"))
+]
