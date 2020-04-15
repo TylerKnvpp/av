3a0fe62f203c375241082d4f4593877c11a0b84f
Tyler Knapp <tgknapp11@gmail.com>
2020-04-15 12:34:21 -0700

    added scrollview for exercise cards

--- av/Home.swift
+++ av/Home.swift
@@ -18,60 +18,39 @@ struct Home: View {
                 .edgesIgnoringSafeArea(.all)
             
             
-            VStack {
-                
-                HStack {
-                    Text("Watching")
-                        .font(.system(size: 28, weight: .bold))
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        self.showProfile.toggle()
-                    }) {
-                                Image("avatar")
-                                    .renderingMode(.original)
-                                    .resizable()
-                                    .frame(width: 36, height: 36)
-                                    .clipShape(Circle())
-                           }
-                }
-                .padding(.horizontal)
-                .padding(.top, 30)
-                Spacer()
-            }
-            .padding(.top, 44)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20 )
-            .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
-            .scaleEffect(showProfile ? 0.9 : 1 )
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-            .edgesIgnoringSafeArea(.all)
+            HomeView(showProfile: $showProfile)
+                .padding(.top, 44)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20 )
+                .offset(y: showProfile ? -450 : 0)
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
+                .scaleEffect(showProfile ? 0.9 : 1 )
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .edgesIgnoringSafeArea(.all)
             
             
             MenuView()
                 .background(Color.black.opacity(0.001))
-                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: showProfile ? 0 : screen.height)
                 .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.64, blendDuration: 0))
                 .onTapGesture {
                     self.showProfile.toggle()
             }
-        .gesture(
-            DragGesture().onChanged { value in
-                self.viewState = value.translation
-            }
-            .onEnded { value in
-                if self.viewState.height > 50 {
-                    self.showProfile = false
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
                 }
-                self.viewState = .zero
-            }
-          )
-        }
-    }
+                .onEnded { value in
+                    if self.viewState.height > 50 {
+                        self.showProfile = false
+                    }
+                    self.viewState = .zero
+                }
+              )
+          }
+     }
 }
 
 struct Home_Previews: PreviewProvider {
@@ -79,3 +58,21 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
+struct AvatarView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.showProfile.toggle()
+        }) {
+            Image("avatar")
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
